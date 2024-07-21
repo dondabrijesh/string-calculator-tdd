0c456538e70c93999c6a3147f20389fd54e38fc5
dondabrijesh <dondabrijesh11@gmail.com>
2024-07-21 19:48:55 +0530

    Condition added for the delimiter // split and scan the numbers

--- string-calc.rb
+++ string-calc.rb
@@ -1,6 +1,13 @@
 def add(numbers)
     return 0 if numbers.empty?
-    # Convert numbers into the array format for 
+    if numbers.start_with?("//")
+        parts = numbers.split("\n", 2) #split the numbers
+        delimiter_section = parts[0][2..-1]
+        delimiters = delimiter_section.scan(/\[(.*?)\]/).flatten
+        numbers = parts[1]
+        delimiters.each { |delim| numbers.gsub!(delim, ",") }
+    end
+    # Convert numbers into the array format for better iteration and manipulation
     number_list = numbers.gsub("\n", ",").split(",").map(&:to_i)
     negatives = number_list.select { |num| num < 0 }
     raise "Negatives not allowed: #{negatives.join(", ")}" unless negatives.empty?
