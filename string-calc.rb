9d5eba4802c1f42f75b0bb9d9015170f6b92e747
dondabrijesh <dondabrijesh11@gmail.com>
2024-07-21 09:29:32 +0530

    String Calc: Negative Number logic added and numbers sum calculation based on input

--- string-calc.rb
+++ string-calc.rb
@@ -1,3 +1,8 @@
 def add(numbers)
     return 0 if numbers.empty?
+    # Convert numbers into the array format for 
+    number_list = numbers.gsub("\n", ",").split(",").map(&:to_i)
+    negatives = number_list.select { |num| num < 0 }
+    raise "Negatives not allowed: #{negatives.join(", ")}" unless negatives.empty?
+    number_list.reduce(:+)
 end
